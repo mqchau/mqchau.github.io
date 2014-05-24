480faa646ffcec14e029676baab7a1e0d3fd741c
mqchau <mqchau@uci.edu>
2014-05-24 15:37:17 -0700

    logo change works OK

--- webpage/js/WebEngine.js
+++ webpage/js/WebEngine.js
@@ -18,15 +18,37 @@ function loadResultPage(){
 
 }
 
+function setRestaurantIcon(name){
+	if (name == "In-N-Out"){
+		document.getElementById("restaurant_logo").src="images/restaurant_logo/In-N-Out-Logo.png";
+	} else 	if (name == "Burger King"){
+		document.getElementById("restaurant_logo").src="images/restaurant_logo/Burger-King-Logo.png";
+	} else 	if (name == "Carl's Jr"){
+		document.getElementById("restaurant_logo").src="images/restaurant_logo/Carls-Jr-Logo.png";
+	} else 	if (name == "Taco Bell"){
+		document.getElementById("restaurant_logo").src="images/restaurant_logo/taco_bell.png";
+	} else 	if (name == "Subway"){
+		document.getElementById("restaurant_logo").src="images/restaurant_logo/Subway-Logo.png";
+	} else 	if (name == "Mc Donalds"){
+		document.getElementById("restaurant_logo").src="images/restaurant_logo/Mcdonalds-Logo.png";
+	}
+	
+}
+
 function populateResultPage(){
 
 	//alert(localStorage.getItem("item1"));
 	document.getElementById("restaurant_name").innerHTML = sessionStorage.getItem("bestRestaurant");
 	
 	//set restaurant icon
-	//setRestaurantIcon(sessionStorage.getItem("bestRestaurant"));
+	setRestaurantIcon(sessionStorage.getItem("bestRestaurant"));
 	
-	//set
+	//set order items
+	var itemList = 	sessionStorage.getItem("resultItemList");
+	
+	for (var item in itemList){
+		
+	}
 	
 }
 
@@ -45,7 +67,19 @@ function doCalculation(){
 	
 	if (budget > 200.0) budget = 200.0;
 	
-	parseDatabase(selection, budget);
+	//parseDatabase(selection, budget);			//we're supposed to call it
+	
+	sessionStorage.setItem("sumCalories", 2000);
+	sessionStorage.setItem("sumFat", 12);
+	sessionStorage.setItem("sumProtein", 56);
+	sessionStorage.setItem("sumCarbs", 78);
+	sessionStorage.setItem("sumSodium", 89);
+	
+	sessionStorage.setItem("sumPrice", 586);
+	sessionStorage.setItem("resultItemList", ["hamburger", "cheeseburger"]);
+	sessionStorage.setItem("bestRestaurant", "Carl's Jr");
+	
+	loadResultPage();
 }
 
 function loadJSON(file_name){
