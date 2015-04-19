7539db5fa0a3b8f201a13bea9e233d4b782f0d0a
Quan Chau <quanqchau@gmail.com>
2015-04-18 23:41:22 -0700

    gah more work

--- engine.js
+++ engine.js
@@ -1,10 +1,30 @@
+var DefaultTotalMonthMinute = 282 * 60;
+
 $(document).ready(function(){
+	//update the total time of month
+	var DefaultTotalMonthTime = convertMinuteToTime(DefaultTotalMonthMinute);
+	$("#TotalTimeInputHour").val(DefaultTotalMonthTime.Hour);
+	$("#TotalTimeInputMinute").val(DefaultTotalMonthTime.Minute);
+
 	//try to get the current date, time and set it to first or 2nd half of the month
 	updateForm();
 
 	//assign callback to the button
 	$("#CalculateButton").click(doCalculation);
+	$("#MonthHalfSelect").change(updateForm);
 });
+
+function convertMinuteToTime(minute){
+	return {
+		Hour: Math.floor(minute / 60),
+		Minute: paddedZero(minute % 60)
+	}
+}
+
+function paddedZero(number){
+	if (number < 10) return "0" + number.toString();
+	else return number.toString();
+}
 
 function updateForm(){
 	//based on whether this is first half or second half of the month, decide to ask the number of time last time
