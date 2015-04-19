1832f8790a25b8d159c5c6c7c4cf6656d6206d27
Quan Chau <quanqchau@gmail.com>
2015-04-18 23:29:07 -0700

    more work

--- engine.js
+++ engine.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	//try to get the current date, time and set it to first or 2nd half of the month
 	updateForm();
+
+	//assign callback to the button
+	$("#CalculateButton").click(doCalculation);
 });
 
 function updateForm(){
@@ -15,4 +18,35 @@ function updateForm(){
 }
 
 
+function doCalculation(){
+	try {
+		//collect data
+		var TotalTime = parseInt($("#TotalTimeInput").val());
+		var MonthHalf = $("#MonthHalfSelect").val();
+		var PreviousTime = 0;
+		if (MonthHalf == 1){
+			//second half
+			PreviousTime = parseInt($("#LastTotalTime").val());
+		}
+		var TotalDayInMonth = parseInt($("#MonthDaySelect").val());
+
+		//process data
+		var TotalTimeThisSection, NumDayThisSection;
+		if (MonthHalf == 0){
+			TotalTimeThisSection = TotalTime / TotalDayInMonth * 15;
+			NumDayThisSection = 15;
+		} else {
+			TotalTimeThisSection = TotalTime - PreviousTime;
+			NumDayThisSection = TotalDayInMonth - 15;
+		}
+
+		var AverageTimeInDay = TotalTimeThisSection / NumDayThisSection;
+
+		//for (var i = 0; i < 
+
+	} catch (e) {
+		alert(e.message);
+	}
+	
+}
 
